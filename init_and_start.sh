e6971c0d39c22a838f7e81b50217adfd64520637
sascha <sascha@ehle.rs>
2021-06-04 15:00:44 +0200

    fix: final dbus settings

--- init_and_start.sh
+++ init_and_start.sh
@@ -1,8 +1,7 @@
 echo "starting dbus"
 
-#dbus-uuidgen > /var/lib/dbus/machine-id
-mkdir -p /var/run/dbus
-dbus-daemon --config-file=/usr/share/dbus-1/system.conf --print-address
+#start if not yet started! this should do the trick ;)
+/etc/init.d/dbus start
 
 echo "starting wpanctl api server"
 
@@ -10,4 +9,29 @@ echo "starting wpanctl api server"
 
 echo "starting wpantund"
 
-/usr/local/sbin/wpantund -o Config:NCP:SocketPath /dev/ttyUSB0 -o Daemon:SyslogMask "all"
+# Syslog mask adjustment. This property is a set of
+# boolean masks for manipulating the bitmask used by `syslog()`.
+# The string can contain the following words that represent
+# the associated bit. The presence of the word in the string
+# indicates that that bit should be set. Prefixing the word with
+# a `-` (dash/minus) indicates that that bit should be cleared.
+# The following keywords are supported:
+#
+# * `all` (All log levels)
+# * `emerg`
+# * `alert`
+# * `crit`
+# * `err`
+# * `warn`
+# * `notice`
+# * `info`
+# * `debug`
+#
+# So, for example, to get all log messages except debugging
+# messages, you would use `all -debug`.
+#
+# Optional. The default value for non-debug builds is
+# `all -info -debug`.
+#
+
+/usr/local/sbin/wpantund -o Config:NCP:SocketPath /dev/ttyUSB0 -o Daemon:SyslogMask "notice"
